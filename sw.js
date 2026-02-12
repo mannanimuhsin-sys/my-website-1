68a74be24691407541d6ce715e346f6b990baf2b
mannanimuhsin-sys <mannanimuhsin@gmail.com>
2026-02-12 10:15:41 +0530

    update data

--- sw.js
+++ sw.js
@@ -1,3 +1,35 @@
-self.addEventListener('fetch', function(event) {
-  // ഇത് ആപ്പിനെ ഇൻസ്റ്റാൾ ചെയ്യാൻ സഹായിക്കുന്ന വെറും ഒരു എംറ്റി സർവീസ് വർക്കർ ആണ്
+const CACHE_NAME = 'madrasa-site-v2'; // ഇവിടെ v1 മാറ്റി v2 ആക്കി
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/madrasa.jpg'
+];
+
+// ഇൻസ്റ്റാൾ ചെയ്യുമ്പോൾ ഫയലുകൾ സേവ് ചെയ്യുന്നു
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// പഴയ കാഷെ ഡിലീറ്റ് ചെയ്യുന്നു (ഇതാണ് പുതിയ അപ്ഡേഷൻ വരാൻ സഹായിക്കുന്നത്)
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// പുതിയ ഡാറ്റ ഉണ്ടെങ്കിൽ അത് എടുക്കുന്നു
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
 });
